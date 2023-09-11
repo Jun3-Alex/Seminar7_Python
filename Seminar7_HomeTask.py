16dbfd9ed7259b4bee4a3ba1976ceb27258b6ec7
Jun3-Alex <130096011+Jun3-Alex@users.noreply.github.com>
2023-09-11 23:50:03 +0500

    dicide second task

--- Seminar7_HomeTask.py
+++ Seminar7_HomeTask.py
@@ -11,7 +11,7 @@
 # пара-ра-рам рам-пам-папам па-ра-па-дам 
 # Вывод:
 # Парам пам-пам
-
+'''
 krichalka = input(f'Введите кричалку: ')
 frase_list = krichalka.split()
 vowels_list = ['а', 'я', 'у', 'ю', 'о', 'е', 'ё', 'э', 'и', 'ы']
@@ -20,5 +20,23 @@ if all(frase.count(vowel) == frase_list[0].count(vowel) for vowel in vowels_list
     print('Парам пам-пам')
 else:
     print('Пам парам')
+'''
 
+# Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6),
+# которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и
+# столбца. Аргументы num_rows и num_columns указывают число строк и столбцов таблицы,
+# которые должны быть распечатаны. Нумерация строк и столбцов идет с единицы (подумайте,
+# почему не с нуля). Примечание: бинарной операцией называется любая операция, у которой
+# ровно два аргумента, как, например, у операции умножения.
+'''
+def print_operation_table(operation, num_rows, num_columns):
+    for i in range(1, num_rows + 1):
+        for j in range(1, num_columns + 1):
+            res = operation(i, j)
+            print(f'{res}\t', end="")
+        # print()
 
+operation = lambda x,y: x*y
+
+print_operation_table(operation, 10, 5)
+'''
